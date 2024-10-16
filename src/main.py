df5a53c218cffe4de4c1abad150f102bd9017dd9
Shishir Rijal <ccrrizal0892@gmail.com>
2024-10-16 15:44:32 +0545

    update main.py

--- src/main.py
+++ src/main.py
@@ -17,7 +17,7 @@ data_dir_path = os.path.join(project_dir, "data")
 print(data_dir_path)
 
 
-def parse_resume(file_path):
+def parse_resume(file_path, job_title):
     # Let's parse the resume first...
     if file_path.endswith(".pdf"):
         raw_text = parse_pdf(file_path)
@@ -36,8 +36,7 @@ def parse_resume(file_path):
     phone_number = extract_phone_number(clean_resume)
     education = extract_education(clean_resume)
     skills = extract_skills_from_resume(clean_resume)
-    job_role = "Data Scientist"
-    job_specific_skills = match_skills_with_job(job_role, clean_resume)
+    job_specific_skills = match_skills_with_job(job_title, clean_resume)
 
     # Print the extracted information in a readable format
     print(f"Name: {name}")
@@ -54,9 +53,9 @@ def parse_resume(file_path):
         for skill in skills:
             print(f"\t- {skill}")
 
-    print(f"Applicant's skills for {job_role}:")
+    print(f"Applicant's skills for {job_title}:")
     if not job_specific_skills:
-        print(f"No skills found for {job_role}")
+        print(f"No skills found for {job_title}")
     else:
         for skill in job_specific_skills:
             print(f"\t- {skill}")
